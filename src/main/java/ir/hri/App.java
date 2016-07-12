b2460b18379d2f145e84baeda5898913edb4e580
HR <ireh.hr@gmail.com>
2016-07-12 19:19:13 +0430

    add New Masseage

--- src/main/java/ir/hri/App.java
+++ src/main/java/ir/hri/App.java
@@ -6,6 +6,7 @@ package ir.hri;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("My name is HamidReza!");
+        System.out.println("My name is HamidReza!!!");
+        System.out.println("Change Source and Commit.");
     }
 }
